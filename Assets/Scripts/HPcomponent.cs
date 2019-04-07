0f2e44267f868fedff6e6daedc0c033bdc79dabd
Penyok8 <kooostya1@gmail.com>
2019-04-07 11:57:03 +0300

    Add character, HPcomponent, MoveComponent

---
+++ Assets/Scripts/HPcomponent.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HPcomponent : MonoBehaviour
+{
+	public float currentHP;
+	public float maxHP;
+
+	public void DoDamage(float dmg)
+	{
+		if (currentHP - dmg > 0)
+		{
+			currentHP -= dmg;
+		}
+		else
+		{
+			currentHP = 0;
+			OnDeath();
+		}
+
+	}
+
+	void OnDeath()
+	{
+
+	}
+}
