383bfd5c702054254687c764d69cbab1d3f0fb90
Валентин Мазурок <valentin.mazurok@gmail.com>
2019-04-07 20:06:15 +0300

    main menu added

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] private AudioSource _bang;
+    [SerializeField] private GameObject _mainMenu;
+    [SerializeField] private GameObject _loadingMenu;
+
+    public void OnPlayClick()
+    {
+        _mainMenu.SetActive(false);
+        _loadingMenu.SetActive(true);
+        Invoke("Load", 3f);
+    }
+
+    private void Load()
+    {
+        SceneManager.LoadScene("level1");
+    }
+    
+    public void OnExitClick()
+    {
+        Application.Quit();
+    }
+}
