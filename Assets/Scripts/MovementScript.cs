d41fbfbf6a1b608b30c7774d8e3c05d4b919b481
Валентин Мазурок <valentin.mazurok@gmail.com>
2019-04-07 18:54:10 +0300

    weapons done

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 public class MovementScript : MonoBehaviour
 {
     [SerializeField] private float _speed;
+    [SerializeField] private Animator _steping;
 
-    // Update is called once per frame
+    private void Start()
+    {
+        _steping.enabled = false;
+    }
+
     void Update()
     {
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         float xmove = Input.GetAxis("Horizontal") * Time.deltaTime;
         float ymove = Input.GetAxis("Vertical") * Time.deltaTime;
+        if (xmove != 0 || ymove != 0)
+            _steping.enabled = true;
+        else
+            _steping.enabled = false;
 
         Vector2 newPos = new Vector3(xmove * _speed, ymove * _speed, 0) + transform.position;
-        
         GetComponent<Rigidbody2D>().MovePosition(newPos);
         GetComponent<Rigidbody2D>().transform.eulerAngles = 
         new Vector3(0, 0,90 + (Mathf.Atan2(mousePos.y - transform.position.y, mousePos.x - transform.position.x) 
