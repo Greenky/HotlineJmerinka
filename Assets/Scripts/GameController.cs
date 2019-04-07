a590ef67bc8ddb50e1e1bc55f83c185b59839298
Penyok8 <kooostya1@gmail.com>
2019-04-07 21:54:02 +0300

    add win/lose screen

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+	public List<HPcomponent> enemys;
+	public HPcomponent player;
+
+	public GameObject winScreen;
+	public GameObject loseScreen;
+
+	public bool gameEnd = false;
+
+	private void Update()
+	{
+		if (gameEnd == false)
+		{
+			if (player == null)
+			{
+				//GAME OVER player lose
+				gameEnd = true;
+				Debug.Log("GAME OVER");
+				loseScreen.SetActive(true);
+			}
+			for (int i = 0; i < enemys.Count; i++)
+			{
+				if (enemys[i] == null)
+					enemys.RemoveAt(i);
+			}
+			if (enemys.Count == 0)
+			{
+				//WIN 
+				gameEnd = true;
+				winScreen.SetActive(true);
+				Debug.Log("PLAYER WIN");
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/ReloadLvl.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReloadLvl : MonoBehaviour
+{
+  public void Reload()
+	{ Scene sc = SceneManager.GetActiveScene();
+		SceneManager.LoadScene(sc.name);
+	}
+}
